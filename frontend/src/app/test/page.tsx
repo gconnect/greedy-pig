1d9034dfafd17f6f0fde6c1fdedcc1401db4f201
Justin Obi <justin@Justins-MacBook-Pro.local>
2024-02-12 14:34:48 +0100

    test convex

--- frontend/src/app/test/page.tsx
+++ frontend/src/app/test/page.tsx
@@ -1,14 +1,31 @@
 "use client";
-import { useQuery } from "convex/react";
+import { useMutation, useQuery } from "convex/react";
 import { api } from "@/convex/_generated/api";
+import { useState } from "react";
 
 export default function Home() {
   const tasks = useQuery(api.tasks.get);
+  const sendTask = useMutation(api.tasks.send);
+
+  const [task, setTask] = useState('')
+
+  const send = async () => {
+
+    await sendTask({ text: task });
+    setTask("");
+  }
+
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {tasks?.map(({ _id, text }) => (
-        <div key={_id}>{text}</div>
-      ))}
-    </main>
+    <div>
+      <main className="flex flex-col items-center justify-between p-24">
+        {tasks?.map(({ _id, text }) => (
+          <div key={_id}>{text}</div>
+        ))}
+      </main>
+
+        <input onChange={e => setTask(e.target.value)} type="text" placeholder="enter task" />
+        <button onClick={send} type="button">Send</button>
+    </div>
   );
 }
--- frontend/src/convex/tasks.ts
+++ frontend/src/convex/tasks.ts
@@ -1,8 +1,17 @@
-import { query } from "./_generated/server";
+import { v } from "convex/values";
+import { mutation, query } from "./_generated/server";
 
 export const get = query({
   args: {},
   handler: async (ctx) => {
     return await ctx.db.query("tasks").collect();
+  },
+});
+
+export const send = mutation({
+  args: { text: v.string() },
+  handler: async (ctx, { text }) => {
+    // Send a new message.
+    await ctx.db.insert("tasks", { text, isCompleted: false });
   },
 });
