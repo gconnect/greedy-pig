582dab8e5b0453f759cfe8fb34740da2262fb420
Justin Obi <justin@Justins-MacBook-Pro.local>
2024-02-26 18:14:52 +0100

    remove viem

--- frontend/src/lib/utils/helpers.ts
+++ frontend/src/lib/utils/helpers.ts
@@ -1,4 +1,4 @@
-import { fromHex } from 'viem'
+import { utils } from 'ethers'
 
 export const shortenAddress = (addr: string) => {
   return `${addr?.substring(0, 6)}...${addr?.substring(addr.length - 4)}`
@@ -8,6 +8,6 @@ export const capitalize = (word: string) =>
   `${word?.substring(0, 1).toUpperCase()}${word?.substring(1)}`
 
 export const parseInputEvent = (input: `0x${string}`) => {
-  let result = fromHex(input, 'string')
+  let result = utils.parseBytes32String(input)
   return JSON.parse(JSON.parse(result))
 }
